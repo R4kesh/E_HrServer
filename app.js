14c58bc366b98a6435293fdedf31427386d13226
Rakesh S <rakeshsrks2580@gmail.com>
2025-01-11 11:37:56 +0530

    chnage

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-// server.js
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import sequelize from './config/db.js';
+
 
 dotenv.config(); // Load environment variables
 
 const app = express();
-app.use(cors());           // Enable CORS
+app.use(cors({origin: 'http://localhost:5173'}));          
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port,async () => {
   console.log(`Server running on port ${port}`);
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 });
